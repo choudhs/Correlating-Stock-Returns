8aa2239da63436591261fee9d433ad255a39131c
saumyachoudhary <38728792+saumyachoudhary@users.noreply.github.com>
2019-09-02 23:49:59 -0700

    Update CorrelatingStockReturns.py

--- CorrelatingStockReturns.py
+++ CorrelatingStockReturns.py
@@ -53,8 +53,8 @@ corr_data_frame.head(10)
 mask = np.zeros_like(corr_data_frame)
 mask[np.triu_indices_from(mask)] = True
 
-# generate plot
-seaborn.heatmap(corr_df, cmap='RdYlGn', vmax=1.0, vmin=-1.0 , mask = mask, linewidths=2.5)
-plt.yticks(rotation=0) 
-plt.xticks(rotation=90) 
+#generate plot
+seaborn.heatmap(corr_df, cmap = 'RdYlGn', vmax = 1.0, vmin = -1.0 , mask = mask, linewidths = 2.5)
+plt.yticks(rotation = 0) 
+plt.xticks(rotation = 90) 
 plt.show()
